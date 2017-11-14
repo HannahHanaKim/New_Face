bb55fc153921b887167becd79572ece0d0ce336d
tjgmlsnla123 <tjgmlsnla123@naver.com>
2017-11-15 01:53:53 +0900

    변경사항 -하나 캐릭터 액티비티xml 창을 삭제하지는 않았지만 그거를 sign up xml에 같이 합쳐서 이미지를 누르면 사진을 선택할 수 있는 창을 보이고 하나의 이미지를 선택하였을 때 원래 이미지 대신 대체되서 나타남 -activity search에서 에뮬레이터로 돌릴 때 공백이 크게 나타나나 핸드폰으로 실행해보았을 때는 멀쩡하게 잘됨 -로그인 창에서 학번과 비밀번호로 로그인할 수 있게 함 앞으로 할 일 -파일에 저장하여 불러올 수 있도록 함 -로그인 시 파일에 학번이 없으면 회원가입 안내 후 회원가입창으로 넘어갈지말지 결정하는 것 -로그인 시 학번과 비밀번호가 다르면 로그인 fail되게 하기 -회원가입시 이미지, 학번, 이름, 비밀번호 파일에 저장 -회원가입하고 넥스트 페이지 버튼 누를 시 search_activity에 학번을 넘겨주고 학번과 어레이를 같이 넘겨줌 ->그래서 그 학번으로 로그인 시 어레이에 저장된 매칭값을 통해서 다른 사람과의 매치율을 계산할 수 있도록 도와줌.

--- app/src/main/java/kr/newface/new_face/new_face/Activity_signup.java
+++ app/src/main/java/kr/newface/new_face/new_face/Activity_signup.java
@@ -6,8 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,11 +21,9 @@ import butterknife.ButterKnife;
 
 public class Activity_signup extends AppCompatActivity {
     private static final String TAG = "SignupActivity";
-
+    RelativeLayout sliding;
     @BindView(R.id.input_name) EditText _nameText;
-    @BindView(R.id.input_address) EditText _addressText;
-    @BindView(R.id.input_email) EditText _emailText;
-    @BindView(R.id.input_mobile) EditText _mobileText;
+    @BindView(R.id.input_studentID) EditText _studentIdText;
     @BindView(R.id.input_password) EditText _passwordText;
     @BindView(R.id.input_reEnterPassword) EditText _reEnterPasswordText;
     @BindView(R.id.btn_signup) Button _signupButton;
@@ -30,13 +33,12 @@ public class Activity_signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-
         ButterKnife.bind(this);
 
         _signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //signup();
+                signup();
             }
         });
 
@@ -50,7 +52,153 @@ public class Activity_signup extends AppCompatActivity {
                 overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
             }
         });
+        sliding = (RelativeLayout)findViewById(R.id.sliding);
+        //이미지 버튼을 누르면 이미지를 고를 수 잇는 창이 나타남
+        final ImageView image = (ImageView)findViewById(R.id.image);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sliding.setVisibility(View.VISIBLE);
+            }
+        });
+
+        ImageView image1, image2, image3, image4, image5, image6;
+        image1 = (ImageView)findViewById(R.id.asian_man);
+        image2 = (ImageView)findViewById(R.id.asian_woman);
+        image3 = (ImageView)findViewById(R.id.black_man);
+        image4 = (ImageView)findViewById(R.id.black_woman);
+        image5 = (ImageView)findViewById(R.id.white_man);
+        image6 = (ImageView)findViewById(R.id.white_woman);
+        //이미지를 선택할 시 이미지 선택창을 다시 안보이게 설정하고 선택한 이미지로 대체
+        image1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                image.setImageDrawable(getResources().getDrawable(R.drawable.asian_man));
+                sliding.setVisibility(View.GONE);
+            }
+        });
+        image2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                image.setImageDrawable(getResources().getDrawable(R.drawable.asian_woman));
+                sliding.setVisibility(View.GONE);
+            }
+        });
+        image3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                image.setImageDrawable(getResources().getDrawable(R.drawable.black_man));
+                sliding.setVisibility(View.GONE);
+            }
+        });
+        image4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                image.setImageDrawable(getResources().getDrawable(R.drawable.black_woman));
+                sliding.setVisibility(View.GONE);
+            }
+        });
+        image5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                image.setImageDrawable(getResources().getDrawable(R.drawable.white_man));
+                sliding.setVisibility(View.GONE);
+            }
+        });
+        image6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                image.setImageDrawable(getResources().getDrawable(R.drawable.white_woman));
+                sliding.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    public void signup() {
+        Log.d(TAG, "Signup");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        _signupButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(Activity_signup.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating Account...");
+        progressDialog.show();
+
+        String name = _nameText.getText().toString();
+        String studentId = _studentIdText.getText().toString();
+        String password = _passwordText.getText().toString();
+        String reEnterPassword = _reEnterPasswordText.getText().toString();
+
+        // TODO: Implement your own signup logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onSignupSuccess or onSignupFailed
+                        // depending on success
+                        onSignupSuccess();
+                        // onSignupFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 1000);
     }
 
 
+    public void onSignupSuccess() {
+        _signupButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        Intent it = new Intent(Activity_signup.this, searchActivity.class);
+        startActivity(it);
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        _signupButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = _nameText.getText().toString();
+        String address = _studentIdText.getText().toString();
+        String password = _passwordText.getText().toString();
+        String reEnterPassword = _reEnterPasswordText.getText().toString();
+
+        if (name.isEmpty() || name.length() < 2) {
+            _nameText.setError("2자 이상이어야 합니다.");
+            valid = false;
+        } else {
+            _nameText.setError(null);
+        }
+
+        if (address.length()!=9||address.isEmpty()) {
+            _studentIdText.setError("학번은 9자 입니다");
+            valid = false;
+        } else {
+            _studentIdText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("4자 이상에서 10자이하의 숫자와 알파벳이어야 합니다.");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        if (reEnterPassword.isEmpty() || reEnterPassword.length() < 4 || reEnterPassword.length() > 10 || !(reEnterPassword.equals(password))) {
+            _reEnterPasswordText.setError("비밀번호가 다릅니다.");
+            valid = false;
+        } else {
+            _reEnterPasswordText.setError(null);
+        }
+
+        return valid;
+    }
 }
--- app/src/main/java/kr/newface/new_face/new_face/searchActivity.java
+++ app/src/main/java/kr/newface/new_face/new_face/searchActivity.java
@@ -1,104 +1,83 @@
 package kr.newface.new_face.new_face;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-
-import butterknife.BindView;
-
 public class searchActivity extends AppCompatActivity {
-  /*  CheckBox s1 = (CheckBox) findViewById(R.id.s1);
-    CheckBox s2 = (CheckBox) findViewById(R.id.s2);
-    CheckBox s3 = (CheckBox) findViewById(R.id.s3);
-    CheckBox s4 = (CheckBox) findViewById(R.id.s4);
-    CheckBox s5 = (CheckBox) findViewById(R.id.s5);
-    CheckBox s6 = (CheckBox) findViewById(R.id.s6);
-    CheckBox s7 = (CheckBox) findViewById(R.id.s7);
-    CheckBox s8 = (CheckBox) findViewById(R.id.s8);
-    CheckBox s9 = (CheckBox) findViewById(R.id.s9);
-    CheckBox s10 = (CheckBox) findViewById(R.id.s10);
-    CheckBox s11 = (CheckBox) findViewById(R.id.s11);
-    CheckBox s12 = (CheckBox) findViewById(R.id.s12);
-    CheckBox s13 = (CheckBox) findViewById(R.id.s13);
-    CheckBox s14 = (CheckBox) findViewById(R.id.s14);
-    CheckBox s15 = (CheckBox) findViewById(R.id.s15);
-    CheckBox s16 = (CheckBox) findViewById(R.id.s16);
-    CheckBox s17 = (CheckBox) findViewById(R.id.s17);
-    CheckBox s18 = (CheckBox) findViewById(R.id.s18);
-    CheckBox s19 = (CheckBox) findViewById(R.id.s19);
-    CheckBox s20 = (CheckBox) findViewById(R.id.s20);
-    CheckBox s21 = (CheckBox) findViewById(R.id.s21);
-    CheckBox s22 = (CheckBox) findViewById(R.id.s22);
-    CheckBox s23 = (CheckBox) findViewById(R.id.s23);
-    CheckBox s24 = (CheckBox) findViewById(R.id.s24);
-    CheckBox s25  = (CheckBox) findViewById(R.id.s25);
-    CheckBox s26 = (CheckBox) findViewById(R.id.s26);
-    CheckBox s27 = (CheckBox) findViewById(R.id.s27);
-    CheckBox s28 = (CheckBox) findViewById(R.id.s28);*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        Button btn = (Button)findViewById(R.id.btn_signup);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(searchActivity.this, Activity_login.class);
+                startActivity(it);
+            }
+        });
 
-        /*int[] search = new int[28];
-        if(s1.isChecked()){search[0]=1;}
-        else{search[0]=0;}
-        if(s2.isChecked()){search[1]=1;}
-        else{search[1]=0;}
-        if(s3.isChecked()){search[2]=1;}
-        else{search[2]=0;}
-        if(s4.isChecked()){search[3]=1;}
-        else{search[3]=0;}
-        if(s5.isChecked()){search[4]=1;}
-        else{search[4]=0;}
-        if(s6.isChecked()){search[5]=1;}
-        else{search[5]=0;}
-        if(s7.isChecked()){search[6]=1;}
-        else{search[6]=0;}
-        if(s8.isChecked()){search[7]=1;}
-        else{search[7]=0;}
-        if(s9.isChecked()){search[8]=1;}
-        else{search[8]=0;}
-        if(s10.isChecked()){search[9]=1;}
-        else{search[9]=0;}
-        if(s11.isChecked()){search[10]=1;}
-        else{search[10]=0;}
-        if(s12.isChecked()){search[11]=1;}
-        else{search[11]=0;}
-        if(s13.isChecked()){search[12]=1;}
-        else{search[12]=0;}
-        if(s14.isChecked()){search[13]=1;}
-        else{search[13]=0;}
-        if(s15.isChecked()){search[14]=1;}
-        else{search[14]=0;}
-        if(s16.isChecked()){search[15]=1;}
-        else{search[15]=0;}
-        if(s17.isChecked()){search[16]=1;}
-        else{search[16]=0;}
-        if(s18.isChecked()){search[17]=1;}
-        else{search[17]=0;}
-        if(s19.isChecked()){search[18]=1;}
-        else{search[18]=0;}
-        if(s20.isChecked()){search[19]=1;}
-        else{search[19]=0;}
-        if(s21.isChecked()){search[20]=1;}
-        else{search[20]=0;}
-        if(s22.isChecked()){search[21]=1;}
-        else{search[21]=0;}
-        if(s23.isChecked()){search[22]=1;}
-        else{search[22]=0;}
-        if(s24.isChecked()){search[23]=1;}
-        else{search[23]=0;}
-        if(s25.isChecked()){search[24]=1;}
-        else{search[24]=0;}
-        if(s26.isChecked()){search[25]=1;}
-        else{search[25]=0;}
-        if(s27.isChecked()){search[26]=1;}
-        else{search[26]=0;}
-        if(s28.isChecked()){search[27]=1;}
-        else{search[27]=0;}*/
+        CheckBox s1 = (CheckBox)findViewById(R.id.s1);
+        CheckBox s2 = (CheckBox)findViewById(R.id.s2);
+        CheckBox s3 = (CheckBox)findViewById(R.id.s3);
+        CheckBox s4 = (CheckBox)findViewById(R.id.s4);
+        CheckBox s5 = (CheckBox)findViewById(R.id.s5);
+        CheckBox s6 = (CheckBox)findViewById(R.id.s6);
+        CheckBox s7 = (CheckBox)findViewById(R.id.s7);
+        CheckBox s8 = (CheckBox)findViewById(R.id.s8);
+        CheckBox s9 = (CheckBox)findViewById(R.id.s9);
+        CheckBox s10 = (CheckBox)findViewById(R.id.s10);
+        CheckBox s11 = (CheckBox)findViewById(R.id.s11);
+        CheckBox s12 = (CheckBox)findViewById(R.id.s12);
+        CheckBox s13 = (CheckBox)findViewById(R.id.s13);
+        CheckBox s14 = (CheckBox)findViewById(R.id.s14);
+        CheckBox s15 = (CheckBox)findViewById(R.id.s15);
+        CheckBox s16 = (CheckBox)findViewById(R.id.s16);
+        CheckBox s17 = (CheckBox)findViewById(R.id.s17);
+        CheckBox s18 = (CheckBox)findViewById(R.id.s18);
+        CheckBox s19 = (CheckBox)findViewById(R.id.s19);
+        CheckBox s20 = (CheckBox)findViewById(R.id.s20);
+        CheckBox s21 = (CheckBox)findViewById(R.id.s21);
+        CheckBox s22 = (CheckBox)findViewById(R.id.s22);
+        CheckBox s23 = (CheckBox)findViewById(R.id.s23);
+        CheckBox s24 = (CheckBox)findViewById(R.id.s24);
+        CheckBox s25 = (CheckBox)findViewById(R.id.s25);
+        CheckBox s26 = (CheckBox)findViewById(R.id.s26);
+        CheckBox s27 = (CheckBox)findViewById(R.id.s27);
+        CheckBox s28 = (CheckBox)findViewById(R.id.s28);
+
+        int[] search = new int[28];
+        if(s1.isChecked()){search[0]=1;} else{search[0]=0;}
+        if(s2.isChecked()){search[1]=1;} else{search[1]=0;}
+        if(s3.isChecked()){search[2]=1;} else{search[2]=0;}
+        if(s4.isChecked()){search[3]=1;} else{search[3]=0;}
+        if(s5.isChecked()){search[4]=1;} else{search[4]=0;}
+        if(s6.isChecked()){search[5]=1;} else{search[5]=0;}
+        if(s7.isChecked()){search[6]=1;} else{search[6]=0;}
+        if(s8.isChecked()){search[7]=1;} else{search[7]=0;}
+        if(s9.isChecked()){search[8]=1;} else{search[8]=0;}
+        if(s10.isChecked()){search[9]=1;} else{search[9]=0;}
+        if(s11.isChecked()){search[10]=1;} else{search[10]=0;}
+        if(s12.isChecked()){search[11]=1;} else{search[11]=0;}
+        if(s13.isChecked()){search[12]=1;} else{search[12]=0;}
+        if(s14.isChecked()){search[13]=1;} else{search[13]=0;}
+        if(s15.isChecked()){search[14]=1;} else{search[14]=0;}
+        if(s16.isChecked()){search[15]=1;} else{search[15]=0;}
+        if(s17.isChecked()){search[16]=1;} else{search[16]=0;}
+        if(s18.isChecked()){search[17]=1;} else{search[17]=0;}
+        if(s19.isChecked()){search[18]=1;} else{search[18]=0;}
+        if(s20.isChecked()){search[19]=1;} else{search[19]=0;}
+        if(s21.isChecked()){search[20]=1;} else{search[20]=0;}
+        if(s22.isChecked()){search[21]=1;} else{search[21]=0;}
+        if(s23.isChecked()){search[22]=1;} else{search[22]=0;}
+        if(s24.isChecked()){search[23]=1;} else{search[23]=0;}
+        if(s25.isChecked()){search[24]=1;} else{search[24]=0;}
+        if(s26.isChecked()){search[25]=1;} else{search[25]=0;}
+        if(s27.isChecked()){search[26]=1;} else{search[26]=0;}
+        if(s28.isChecked()){search[27]=1;} else{search[27]=0;}
     }
 }
