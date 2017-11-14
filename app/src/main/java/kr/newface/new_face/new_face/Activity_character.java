70ce793df7cbf8e9bc95e2c27afb33b56e689904
HannahHanaKim <32616606+HannahHanaKim@users.noreply.github.com>
2017-11-14 17:32:38 +0900

    activity_character: add character pictures w/ no actions yet

---
+++ app/src/main/java/kr/newface/new_face/new_face/Activity_character.java
@@ -0,0 +1,15 @@
+package kr.newface.new_face.new_face;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_character extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character);
+
+
+    }
+}
