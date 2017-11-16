57c8daa6d05415803652af0a5158b8fe309c4477
tjgmlsnla123 <tjgmlsnla123@naver.com>
2017-11-16 18:03:22 +0900

    activity signup에 텍스트뷰 추가

--- app/src/main/java/kr/newface/new_face/new_face/Activity_login.java
+++ app/src/main/java/kr/newface/new_face/new_face/Activity_login.java
@@ -1,16 +1,12 @@
 package kr.newface.new_face.new_face;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -77,8 +73,6 @@ public class Activity_login extends AppCompatActivity {
         return valid;
     }
 
-
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_SIGNUP) {
@@ -96,6 +90,4 @@ public class Activity_login extends AppCompatActivity {
         // Disable going back to the MainActivity
         moveTaskToBack(true);
     }
-
-
 }
