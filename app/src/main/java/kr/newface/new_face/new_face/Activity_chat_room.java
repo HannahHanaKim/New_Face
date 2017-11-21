3c07c20c4da712f9e2df025fb05bdcfa5f3f0e7a
Jung_Mac <iveinvalue@gmail.com>
2017-11-21 17:21:47 +0900

    ^^

--- app/src/main/java/kr/newface/new_face/new_face/Activity_chat_room.java
+++ app/src/main/java/kr/newface/new_face/new_face/Activity_chat_room.java
@@ -10,12 +10,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.github.bassaer.chatmessageview.models.Message;
 import com.github.bassaer.chatmessageview.models.User;
 import com.github.bassaer.chatmessageview.utils.ChatBot;
 import com.github.bassaer.chatmessageview.views.ChatView;
+
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -25,10 +28,23 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import cz.msebera.android.httpclient.HttpEntity;
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.NameValuePair;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.entity.UrlEncodedFormEntity;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import cz.msebera.android.httpclient.message.BasicNameValuePair;
+import cz.msebera.android.httpclient.util.EntityUtils;
 
 import static kr.newface.new_face.new_face.MainActivity.my_id;
 
@@ -44,7 +60,9 @@ public class Activity_chat_room extends AppCompatActivity {
 
     User me;
     User you;
+    String name;
 
+    @BindView(R.id.switch2) Switch switch2;
     @BindView(R.id.chat_view) ChatView mChatView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,34 +70,41 @@ public class Activity_chat_room extends AppCompatActivity {
         setContentView(R.layout.activity_chat_room);
 
         ButterKnife.bind(this);
-        init();
+
 
         Intent intent = getIntent();
-        final String name = intent.getExtras().getString("name");
+        name = intent.getExtras().getString("name");
 
-
+        init();
 
         //전송 버튼 눌렀을때
         mChatView.setOnClickSendButtonListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String temp_str = mChatView.getInputText();
+
+                if (switch2.isChecked() == true){
+                    temp_str = gethttp(temp_str).split("translatedText\":\"")[1].split("\"")[0];
+                }
+
+                //Toast.makeText(getApplication(), gethttp(""), Toast.LENGTH_SHORT).show();
                 Message message = new Message.Builder()
                         .setUser(me)
                         .setRightMessage(true)
-                        .setMessageText(mChatView.getInputText())
+                        .setMessageText(temp_str)
                         .hideIcon(true)
                         .build();
                 mChatView.send(message);
 
                 try {
                     PrintWriter out = new PrintWriter(outToServer, true);
-                    out.println(mChatView.getInputText());
+                    out.println(temp_str);
                 } catch (Exception e) {
 
                 }
 
 
-                Toast.makeText(getApplication(), "\"" + my_id + "\",\"" + name + "\",\"" + mChatView.getInputText() + "\"", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplication(), temp_str, Toast.LENGTH_SHORT).show();
                 mChatView.setInputText("");
             }
 
@@ -115,9 +140,11 @@ public class Activity_chat_room extends AppCompatActivity {
     };
 
     void init(){
+        getSupportActionBar().hide();
+        getSupportActionBar().setElevation(0);
         try{
             //나중에 켜야됨
-            //clientSocket = new Socket("219.248.3.152", 6788);
+            //clientSocket = new Socket("192.9.81.159", 9001);
 
             outToServer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
             inFromServer =  new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
@@ -125,16 +152,27 @@ public class Activity_chat_room extends AppCompatActivity {
             checkUpdate.start();
             mHandler = new Handler();
 
-            try {
-                PrintWriter out = new PrintWriter(outToServer, true);
-                out.println("\"" + my_id + "\"");
-            } catch (Exception e) {
 
-            }
             //clientSocket.close();
         }catch (Exception e){
             Toast.makeText(getApplication(), "서버 오류", Toast.LENGTH_SHORT).show();
         }
+
+        if (name.contains("채팅방")){
+            Toast.makeText(getApplication(), "\"" + my_id + "\"/\"" + name.split("채팅방")[1] + "\"", Toast.LENGTH_SHORT).show();
+
+        }else{
+            Toast.makeText(getApplication(), "\"" + my_id + "\"/\"-1\"", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplication(), "\"" + name + "\"", Toast.LENGTH_SHORT).show();
+        }
+        try {
+            PrintWriter out = new PrintWriter(outToServer, true);
+            out.println("\"" + my_id + "\"");
+        } catch (Exception e) {
+
+        }
+
+
 
         //채팅방 디자인 설정
         mChatView.setRightBubbleColor(ContextCompat.getColor(this, R.color.green500));
@@ -154,5 +192,39 @@ public class Activity_chat_room extends AppCompatActivity {
         //사용자 설정
         me = new User(0, "나", BitmapFactory.decodeResource(getResources(), R.drawable.profile));
         you = new User(1, "상대방", BitmapFactory.decodeResource(getResources(), R.drawable.profile));
+    }
+
+    public String gethttp(String sstr1) {
+        try {
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+            nameValuePairs.add(new BasicNameValuePair("source", "en"));
+            nameValuePairs.add(new BasicNameValuePair("target", "ko"));
+            nameValuePairs.add(new BasicNameValuePair("text", sstr1));
+
+
+            HttpClient client = new DefaultHttpClient();
+            HttpPost post = new HttpPost();
+            post.setURI(new URI("https://openapi.naver.com/v1/language/translate"));
+            //post.setHeader("HOST","openapi.naver.com");
+            post.setHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8");
+            post.setHeader("X-Naver-Client-Id","I7xF1aoEPAUZElXdHSH1");
+            post.setHeader("X-Naver-Client-Secret","MhLdSaa8lj");
+            post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            HttpResponse resp = client.execute(post);
+            BufferedReader br = new BufferedReader(new InputStreamReader(resp.getEntity().getContent()));
+            String str = null;
+            StringBuilder sb = new StringBuilder();
+            while ((str = br.readLine()) != null) {
+                sb.append(str).append("\n");
+            }
+            br.close();
+            return sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+
+
     }
 }
